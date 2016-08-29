94a377147cf49f7094e84d14c7c3162997592a36
Roman Savin <savin9201@gmail.com>
2016-08-29 19:32:03 +0300

    Add FailingWordsPresenter interface.

---
+++ app/src/main/java/com/romansavin/fallingwords/presenter/FallingWordsPresenter.java
@@ -0,0 +1,14 @@
+package com.romansavin.fallingwords.presenter;
+
+/**
+ * @author Roman Savin
+ * @since 29.08.2016.
+ */
+public interface FallingWordsPresenter {
+
+  void startGame();
+
+  void chooseAnswerRight();
+
+  void chooseAnswerWrong();
+}
