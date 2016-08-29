a9adb215b97e31b375daff4be52523009aad7a33
Roman Savin <savin9201@gmail.com>
2016-08-29 20:41:50 +0300

    Add FallingWordsApplication.

---
+++ app/src/main/java/com/romansavin/fallingwords/FallingWordsApplication.java
@@ -0,0 +1,17 @@
+package com.romansavin.fallingwords;
+
+import android.app.Application;
+
+/**
+ * @author Roman Savin
+ * @since 29.08.2016.
+ */
+public class FallingWordsApplication extends Application {
+
+  public static final String LOG_TAG = "FallingWords";
+
+  @Override public void onCreate() {
+    super.onCreate();
+    //TODO implement
+  }
+}
